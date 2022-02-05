3c9755f613ff43123644f2ae114c83104ab633d9
Gary Malcolm Bleasdale <g.m.bleasdale@gmail.com>
2022-02-05 11:32:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let nextButton = document.body.querySelector('.btn2')
+let previousButton=document.body.querySelector('.btn1')
+let lines = document.body.querySelectorAll('.line')
+let circles = document.body.querySelectorAll('.circle')
+console.log(circles.length)
+
+console.log(circles[3])
+
+nextButton.addEventListener("click", function(){
+for(i=0;i<circles.length-1;i++){
+  if (lines[i].classList.contains("active")===false ){
+    circles[i+1].classList.add('active');
+    lines[i].classList.add('active')
+    break
+  } 
+
+}
+if(lines[2].classList.contains("active")){
+  nextButton.innerText='Finish'
+}
+
+})
+
+previousButton.addEventListener("click", function(){
+  for(i=circles.length-1;i>=0;i--){
+    if (circles[i].classList.contains("active")){
+      circles[i].classList.remove('active');
+      lines[i-1].classList.remove('active')
+      nextButton.innerText='Next'
+      break
+    }
+  }
+
+  
+  })
+
